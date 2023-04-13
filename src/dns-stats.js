21e80cc2672fd73c21961356a14157ca56585400
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-04-14 02:03:39 +0300

    dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(/* domains */ domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const domains_address = domains.map((el) => el.split(".").reverse());
+  const out = {};
+  for (const domain of domains_address) {
+    for (let i = 0; i < domain.length; i++) {
+      const key = ".".concat(domain.slice(0, i + 1).join("."));
+      out[key] ? (out[key] += 1) : (out[key] = 1);
+    }
+  }
+  return out;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
